3c6d475742f7ef66eb82a340e9c305db40ecce84
Andy Molinaro <andrew.molinaro916@gmail.com>
2023-03-19 03:16:36 -0500

    created the 16x16 divs, also added mouseover functionality

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-console.log("File Loaded")
+const containerDiv = document.querySelector("#container")
+let grid = 0;
 
+function rowMaker(rows, cols) {
+    containerDiv.style.setProperty("--grid-rows", rows);
+    containerDiv.style.setProperty("--grid-columns", cols);
+    for (i = 0; i < (rows * cols); i++ ){
+        let sq = document.createElement("div"); 
+        containerDiv.appendChild(sq).className = "grid-item";
+        sq.addEventListener("mouseover", () => {
+            sq.style.backgroundColor = "black";
+        })
+    }
+
+}
+
+rowMaker (16, 16)
